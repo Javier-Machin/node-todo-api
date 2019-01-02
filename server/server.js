f89aafcb2836f3086a65011590d0301c1a40d151
Javier Machin <javier_machin_acosta@hotmail.com>
2019-01-02 15:38:25 +0000

    Add mongoose and Todo model

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+const Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: { 
+    type: Boolean 
+  },
+  completedAt: {
+    type: Number 
+  }
+});
+
+const newTodo = new Todo({
+  text: 'Cook dinner'
+})
+
+newTodo.save().then((doc) => {
+  console.log('Saved todo', doc);
+}, (e) => {
+  console.log('Unable to save todo', e);
+});
+
+
+const otherTodo = new Todo({
+  text: 'Walk the dog',
+  completed: false,
+  completedAt: Date.parse(new Date)
+});
+
+otherTodo.save().then((doc) => {
+  console.log('Saved todo', doc);
+}, (e) => {
+  console.log('Unable to save todo', e);
+})
