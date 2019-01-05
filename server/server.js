6a8f414ae2b33d7328de4a20b5f061f260d2b456
Javier Machin <javier_machin_acosta@hotmail.com>
2019-01-05 12:57:31 +0000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -33,6 +34,35 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+
+// /todos/:id GET route, get one todo
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  // Validate id using isValid
+    // if invalid, status 404 send empty send
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+  // findbyId
+  Todo.findById(id).then((todo) => {
+    // if no todo - send back 404 with empty body
+    if (!todo) {
+      return res.status(404).send();
+    }
+   
+    // if todo - send it back
+    res.send({ todo });
+    
+    // error
+    // 400 - and send empty body back
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
