88587172aa07ebc9bc59dfa2a40a9abaa03ac2be
Javier Machin <javier_machin_acosta@hotmail.com>
2019-01-09 13:35:40 +0000

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 
@@ -155,5 +157,46 @@ describe('DELETE /todos/:id', () => {
       .delete(`/todos/${validNonExistentId}`)
       .expect(404)
       .end(done);
+  });
+});
+
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    const validId = todos[0]._id.toHexString();
+    
+    const data = {
+      text: 'updated text',
+      completed: true
+    }
+
+    request(app)
+      .patch(`/todos/${validId}`)
+      .send(data)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toNotBe(todos[0].text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      }).end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    const validId = todos[1]._id.toHexString();
+    
+    const data = {
+      text: 'updated text',
+      completed: false
+    } 
+
+    request(app)
+    .patch(`/todos/${validId}`)
+    .send(data)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toNotBe(todos[1].text);
+      expect(res.body.todo.completed).toBe(false);
+      expect(res.body.todo.completedAt).toNotExist();
+    }).end(done);
   });
 });
