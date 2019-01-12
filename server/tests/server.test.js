9beac8f6acf938865368ffa25b5fd7ba960f1537
Javier Machin <javier_machin_acosta@hotmail.com>
2019-01-12 12:37:21 +0000

    Add user seeds for testing, move seeds to their own file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,25 +4,10 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-// todos seeds
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
-}];
-
-
-beforeEach((done) => {
-  // previously .remove, now deprecated
-  Todo.deleteMany({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
